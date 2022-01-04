00b540197d99cad6e6162327bc1205529de15e7d
Vincent <vincentzhang@outlook.es>
2022-01-04 22:46:04 +0100

    add in 0104

---
+++ src/main/java/interview/lambda/Item.java
@@ -0,0 +1,16 @@
+package interview.lambda;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+    private String name;
+    private int qty;
+    private BigDecimal price;
+}
